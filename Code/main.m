11ae865d8a303e0c38460de40536f10df02ac6ef
ezion2 <99609015+ezion2@users.noreply.github.com>
2023-11-02 22:46:44 -0700

    Add files via upload

--- Code/main.m
+++ Code/main.m
@@ -58,35 +58,34 @@ function manualControl()
 end
 
 function colorDetection(brick)
-    brick.SetColorMode(1, 4)
-    color_rgb = brick.ColorRGB(1)
-    fprintf("\tRed: %d\n", color_rgb(1));
-    fprintf("\tGreen: %d\n", color_rgb(2));
-    fprintf("\tBlue: %d\n", color_rgb(3));
+    brick.SetColorMode(1, 2)
+    %color_rgb = brick.ColorRGB(1)
+    %fprintf("\tRed: %d\n", color_rgb(1));
+    %fprintf("\tGreen: %d\n", color_rgb(2));
+    %fprintf("\tBlue: %d\n", color_rgb(3));
 
-    while true
-        brick.MoveMotor('A', -10);
-        brick.MoveMotor('B', 10);
-        color = brick.ColorCode(SensorPort);
-        case (color == 5)
-            disp('RED');
-            pause(1);
-        end
+    brick.StopMotor('A');
+    brick.StopMotor('B');
+    
+    color = brick.ColorCode(1);
+    case (color == 5)
+        disp('RED');
+        pause(1);
+    end
 
-        case (color == 2)
-            disp('BLUE');
-            pause(0.1);
-            brick.beep();
-            brick.beep();
-        end
+    case (color == 2)
+        disp('BLUE');
+        pause(0.1);
+        brick.beep();
+        brick.beep();
+    end
 
-        case (color == 3)
-            disp('GREEN');
-            pause(0.1);
-            brick.beep();
-            brick.beep();
-            brick.beep();
-        end
+    case (color == 3)
+        disp('GREEN');
+        pause(0.1);
+        brick.beep();
+        brick.beep();
+        brick.beep();
     end
 
 end
